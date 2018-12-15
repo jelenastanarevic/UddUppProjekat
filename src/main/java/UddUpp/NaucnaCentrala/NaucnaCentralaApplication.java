0bc0a56492990c4cdbe3c25f2357b7a4f6811691
jelenastanarevic <jelena_stanarevic@uns.ac.rs>
2018-12-15 11:30:00 +0100

    Initial commit

---
+++ src/main/java/UddUpp/NaucnaCentrala/NaucnaCentralaApplication.java
@@ -0,0 +1,14 @@
+package UddUpp.NaucnaCentrala;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaucnaCentralaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaucnaCentralaApplication.class, args);
+	}
+
+}
+
